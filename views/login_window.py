7fbe8a8e76127bf733489d8f726e4d7de9f547e4
lauar-m <lauar.m@proton.me>
2025-06-09 19:56:16 -0300

    pt 2

---
+++ views/login_window.py
@@ -0,0 +1,38 @@
+import flet as ft
+from utils.components import create_button, create_card, PRIMARY_COLOR
+
+def LoginWindow(content: ft.Column, on_success):
+    nome_usuario = ft.TextField(label="Nome de usuário", width=300)
+    senha = ft.TextField(label="Senha", password=True, can_reveal_password=True, width=300)
+
+    warning = ft.Text("Preencha todos os campos.", color="red", size=12, visible=False)
+
+    def fazer_login(e):
+        if nome_usuario.value.strip() and senha.value.strip():
+            warning.visible = False
+            on_success(nome_usuario.value.strip())
+        else:
+            warning.visible = True
+        content.update()
+
+    form_login = ft.Column(
+        spacing=20,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[
+            nome_usuario,
+            senha,
+            warning,
+            create_button("Entrar", ft.Icons.LOGIN, action=fazer_login, largura=200, color=PRIMARY_COLOR)
+        ]
+    )
+
+    card = create_card("🔐 Login", form_login)
+
+    content.controls.append(
+        ft.Column(
+            expand=True,
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[card]
+        )
+    )
--- views/played_games_window.py
+++ views/played_games_window.py
@@ -1,17 +1,17 @@
 import flet as ft
-from utils.components import criar_card, COR_SECUNDARIA
+from utils.components import create_card, SECONDARY_COLOR
 
-def tela_jogos(content: ft.Column):
+def PlayedGamesWindow(content: ft.Column):
     historico = ft.Column([
         ft.ListTile(
-            leading=ft.Icon(ft.Icons.EXTENSION, color=COR_SECUNDARIA),
+            leading=ft.Icon(ft.Icons.EXTENSION, color=SECONDARY_COLOR),
             title=ft.Text("Nível Difícil"),
             subtitle=ft.Text("Tempo: 04:32 - 15/06/2023"),
         ),
         ft.ListTile(
-            leading=ft.Icon(ft.Icons.EXTENSION, color=COR_SECUNDARIA),
+            leading=ft.Icon(ft.Icons.EXTENSION, color=SECONDARY_COLOR),
             title=ft.Text("Nível Médio"),
             subtitle=ft.Text("Tempo: 03:18 - 10/06/2023"),
         )
     ])
-    content.controls.append(criar_card("📊 Seu Histórico", historico))
+    content.controls.append(create_card("📊 Seu Histórico", historico))
